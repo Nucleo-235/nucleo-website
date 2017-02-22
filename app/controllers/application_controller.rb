daf3882ae26444a871e449a2f033004cf0666bc0
Henrique Rangel <hrangel@nucleo235.com.br>
2017-02-22 18:37:00 -0300

    atualizados métodos de idiomas para não causar erros quando entram em end errado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_locale
-  before_action :persist_locale
-  before_action :authenticate_user!
-  before_action :set_editor_config
-  before_action :set_localizable_page
+  before_action :set_locale, unless: :devise_controller?
+  before_action :persist_locale, unless: :devise_controller?
+  before_action :authenticate_user!, unless: :devise_controller?
+  before_action :set_editor_config, unless: :devise_controller?
+  before_action :set_localizable_page, unless: :devise_controller?
 
   def after_sign_up_path_for(resource)
     root_path
@@ -101,22 +101,32 @@ class ApplicationController < ActionController::Base
     end
 
     def redirect_to_locale_if_not_set
-        if params[:locale]
-          I18n.locale = params[:locale]
-          # current_user_or_visitor.update(locale: I18n.locale.to_s)
-        else
-          locale = request_locale || I18n.default_locale
-          redirect_to url_for(request.params.merge({ locale: locale }))
+      if params[:locale]
+        begin
+          I18n.locale = string_to_locale(params[:locale]).to_s
+        rescue
+          redirect_to url_for(request.params.merge({ locale: I18n.default_locale }))
         end
+        # current_user_or_visitor.update(locale: I18n.locale.to_s)
+      else
+        locale = request_locale || I18n.default_locale
+        redirect_to url_for(request.params.merge({ locale: locale }))
       end
+    end
 
     def get_locale
       # params[:locale] || visitor_locale || request_locale || I18n.default_locale
-      params[:locale] || request_locale || I18n.default_locale
+      params[:locale] ? string_to_locale(params[:locale]) : (session[:locale] || request_locale || I18n.default_locale)
     end
 
     def set_locale
-      I18n.locale = get_locale
+      current_locale = get_locale.to_sym
+      if I18n.available_locales.include? current_locale
+        I18n.locale = current_locale
+        session[:locale] = current_locale
+      else
+        redirect_to url_for(request.params.merge({ locale: I18n.default_locale }))
+      end
     end
 
     def persist_locale
@@ -128,6 +138,21 @@ class ApplicationController < ActionController::Base
       locale = http_accept_language.preferred_language_from(I18n.available_locales + extra_locales)
       locale = 'pt-BR' if locale == :pt || locale.to_s.downcase == 'pt-pt' || locale.to_s.downcase == 'pt-br'
       locale
+    end
+
+    def default_url_options(options = {})
+      args = { locale: get_locale }
+      # logger.debug args.to_json
+      args
+    end
+
+    def string_to_locale(value)
+      locale_values = value.split('-')
+      if locale_values.length == 2
+        return "#{locale_values[0]}-#{locale_values[1].upcase}"
+      else
+        return locale_values[0]
+      end
     end
 
     def set_editor_config
