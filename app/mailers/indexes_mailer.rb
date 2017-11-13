c9ddc86d09e3afc0045df265c80e7202ebd45492
Henrique Rangel <hrangel@hrangel.com.br>
2017-11-13 13:13:55 -0200

    corrigido assunto de email de indices

--- app/mailers/indexes_mailer.rb
+++ app/mailers/indexes_mailer.rb
@@ -2,6 +2,6 @@ class IndexesMailer < ApplicationMailer
   def list(indexes)
     @indexes = indexes
     mail_from = ENV['MAILER_FROM'] || 'kikecomp@gmail.com'
-    mail(from: "Nucleo Índices <#{mail_from}>", to: ENV['ADMIN_MAIL'], subject: "Níucleo Índices")
+    mail(from: "Nucleo Índices <#{mail_from}>", to: ENV['ADMIN_MAIL'], subject: "Nucleo Índices")
   end
 end
