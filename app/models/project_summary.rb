e875c3ccdcc5e4ec152490b15dad6ece99b540c3
Henrique Rangel <hrangel@hrangel.com.br>
2017-12-18 10:22:09 -0200

    corrigido script para incluir nova coliuna

--- app/models/project_summary.rb
+++ app/models/project_summary.rb
@@ -1,5 +1,6 @@
 class ProjectSummary
   attr_accessor :name
+  attr_accessor :allowed_extra_hours
   attr_accessor :total_hours
   attr_accessor :planned_hours
   attr_accessor :used_hours
@@ -11,11 +12,12 @@ class ProjectSummary
     item = ProjectSummary.new
     item.name = row[0]
     item.total_hours = row[1].to_f
-    item.planned_hours = row[2].to_f
-    item.used_hours = row[3].to_f
-    item.available_hours = row[4].to_f
-    item.due_date = row[5].to_date
-    item.delivered_at = !row[6] || row[6] == "" ? nil : row[6].to_date
+    item.allowed_extra_hours = row[2].to_f
+    item.planned_hours = row[3].to_f
+    item.used_hours = row[4].to_f
+    item.available_hours = row[5].to_f
+    item.due_date = row[6].to_date
+    item.delivered_at = !row[7] || row[67 == "" ? nil : row[7].to_date
     # puts item.to_json
     item
   end
