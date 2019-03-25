823369bc4bf791838b5918f50e47a0f46fc193fe
Henrique Rangel <hrangel@nucleo235.com.br>
2019-03-25 10:43:25 -0300

    configurada rota de contato

--- app/controllers/general_contacts_controller.rb
+++ app/controllers/general_contacts_controller.rb
@@ -1,23 +1,41 @@
 class GeneralContactsController < ApplicationController
-  skip_before_action :show_analytics
-  skip_before_action :set_locale
-  skip_before_action :persist_locale
+  # skip_before_action :show_analytics
+  # skip_before_action :set_locale
+  # skip_before_action :persist_locale
+  # skip_before_action :authenticate_user!
+  # skip_before_action :set_editor_config
+  # skip_before_action :set_localizable_page
+
   skip_before_action :authenticate_user!
-  skip_before_action :set_editor_config
-  skip_before_action :set_localizable_page
+  skip_before_action :set_locale, only: [:create]
+  skip_before_action :persist_locale, only: [:create]
+  before_action :redirect_to_locale_if_not_set, only: [:create]
 
   def create
-    @contact = GeneralContact.new(params[:general_contact])
-    @contact.request = request
-    if @contact.deliver
+    # Set home info
+    @partners = Partner.all
+
+    @people = Person.where.not(type: Partner.name)
+
+    @projects = Project.all.order(:sort_order)
+    if @can_edit
+      @categories = Category.all
+      @new_project = Project.new
+    end
+
+    @general_contact = GeneralContact.new(params[:general_contact])
+    @general_contact.request = request
+
+    if @general_contact.deliver
+      @general_contact = GeneralContact.new
+      
       puts 'Obrigado por enviar sua mensagem. Entraremos em contato em breve!'
       flash.now[:notice] = 'Obrigado por enviar sua mensagem. Entraremos em contato em breve!'
-      redirect_to root_path, notice: 'Obrigado por enviar sua mensagem. Entraremos em contato em breve!'
     else
       puts 'Não foi possível enviar a mensagem.'
       puts @contact.errors.to_json
       flash.now[:error] = 'Não foi possível enviar a mensagem.'
-      redirect_to root_path, error: 'Não foi possível enviar a mensagem.'
     end
+    render :template => "pages/home"
   end
 end
